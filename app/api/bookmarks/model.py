0d9c363995f2a58f3ee36e13b9b79d986e1b1920
David Manjavacas <david.manjavacas@lastminute.com>
2021-04-07 21:07:23 +0200

    Remove old code

--- app/api/bookmarks/model.py
+++
@@ -1,7 +0,0 @@
-from ..common.db import Document
-from ..common.serializers import ModelSerializerMixin
-
-
-class Bookmark(Document, ModelSerializerMixin):
-    def __init__(self):
-        super().__init__('bookmarks')
