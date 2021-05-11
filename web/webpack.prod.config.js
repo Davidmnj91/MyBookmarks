f6e8d3d0adb1a7be3553e8545d11ae43294aa719
David Manjavacas <david.manjavacas@lastminute.com>
2021-05-11 19:31:13 +0200

    Remove duplicate webpack config

--- web/webpack.prod.config.js
+++ web/webpack.prod.config.js
@@ -22,10 +22,7 @@ module.exports = {
         test: /\.(ts|js)x?$/i,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
-          },
+          loader: 'babel-loader'
         },
       },
     ],
